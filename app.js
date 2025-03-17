1242b8d77950b2ca93dd6fa890923e10c1b9dff2
sun4205 <hanna4205@gmail.com>
2025-03-17 12:29:51 -0400

    add errorhandler

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 const express = require('express');
+const { errors } = require("celebrate");
+const mainRouter = require('./routes/index');
+const errorHandler = require('./middlewares/error-handler');
 
 const app = express();
 
 const {PORT = 3000} = process.env;
+
+app.use(express.json());
+app.use("/", mainRouter);
+app.use(errors());
+app.use(errorHandler);
 
 app.listen(PORT, ()=>{
     console.log(`listening on port ${PORT}`);
