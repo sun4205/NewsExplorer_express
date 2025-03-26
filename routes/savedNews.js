0863dec2d28de382f638f109565b27618ec0374c
sun4205 <hanna4205@gmail.com>
2025-03-26 15:44:28 -0400

    update articleId

--- routes/savedNews.js
+++ routes/savedNews.js
@@ -10,13 +10,14 @@ const {
 router.get("/saveNews", auth, getSavedArticles);
 
 router.put("/saveNews/:id", auth, (req, res, next) => {
+    console.log(req.params.id)
   const articleId = decodeURIComponent(req.params.id);
   req.params.id = articleId;
   savedArticle(req, res, next);
 });
 router.delete("/saveNews/:id", auth, (req, res, next) => {
-  const decodedId = decodeURIComponent(req.params.id);
-  req.params.id = decodedId;
+  const articleId = decodeURIComponent(req.params.id);
+  req.params.id = articleId;
   deleteArticle(req, res, next);
 });
 
