5d1e540fb84689e152fa42e3b5adb73a0774a386
sun4205 <hanna4205@gmail.com>
2025-04-11 21:17:39 -0400

    update code

--- controller/apiNews.js
+++ controller/apiNews.js
@@ -1,20 +1,21 @@
-const fetch = require("node-fetch");  
-const API_KEY = process.env.NEWS_API_KEY;  
+const fetch = require("node-fetch");
+const API_KEY = process.env.NEWS_API_KEY;
 
 exports.getNews = async (req, res) => {
-  const { q } = req.query;  
+  const { q } = req.query;
   try {
-    
-    const response = await fetch(`https://newsapi.org/v2/everything?q=${q}&apiKey=${API_KEY}`);
+    const response = await fetch(
+      `https://newsapi.org/v2/everything?q=${q}&apiKey=${API_KEY}`
+    );
     const data = await response.json();
 
     if (data.status === "ok") {
-      res.json(data); 
+      res.json(data);
     } else {
       res.status(400).json({ message: "Error fetching news" });
     }
   } catch (error) {
     console.error("Error fetching news:", error);
-    res.status(500).json({ message: "Failed to fetch news" }); 
+    res.status(500).json({ message: "Failed to fetch news" });
   }
 };
